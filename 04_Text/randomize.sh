b83a817a000da7db8c3bb9d6d025d7a5e1c8f579
Lorien2027 <61617389+Lorien2027@users.noreply.github.com>
2021-10-14 13:32:25 +0300

    Update 04 task solution

--- 04_Text/randomize.sh
+++ 04_Text/randomize.sh
@@ -6,22 +6,25 @@ if [ -z "$1" ]
     sleep_time=$1
 fi
 
-data=()
+
 length=0
 while IFS= read -r line; do
+    line=$(tr -d '\n' <<< $line)
     for ((i = 0; i < ${#line}; i++)); do
         if ! [ -z ${line:i:1} ]
             then
-                data+=(${line:i:1},$length,$i)
+                data="$data ${line:i:1}\n$length\n$i"
             fi
     done
     length=$((length+1))
 done
 
 tput clear
-for i in $(shuf -e $(seq 0 $((${#data[@]}-1)))); do IFS="," read char height width <<<${data[$i]};
+for i in $(shuf -e $data); do
+    i=$(echo -e "$i" | tr '\n' ' ')
+    IFS=$" " read char height width <<< $i
     tput cup $height $width
     sleep $sleep_time
-    echo -e $char
+    echo -e "$char"
 done 
 tput cup $length 0
