d6e84a0f3b08ccda33467d10e81b1e1f91a3ceb8
Lorien2027 <61617389+Lorien2027@users.noreply.github.com>
2021-10-24 21:46:56 +0300

    Fix indents

--- 05_Debugging/range.c
+++ 05_Debugging/range.c
@@ -2,10 +2,10 @@
 
 int main(int argc, char* argv[]) {
     int start = 0;
-	int end = 1;
-	int step = 1;
-	if (argc == 1) {
-    	printf("%s",
+    int end = 1;
+    int step = 1;
+    if (argc == 1) {
+        printf("%s",
             "usage: range [start] [end] [step]\n"
             "start: range start\n"
             "end: range end\n"
@@ -14,21 +14,21 @@ int main(int argc, char* argv[]) {
             "\t1: [0, 1, ..., end-1]\n"
             "\t2: [start, start+1, ..., end-1]\n"
             "\t3: [start, start+step, ..., end-1]\n"
-    	);
-    	return 0;
+        );
+        return 0;
     }
     else if (argc == 2) {
-     	sscanf(argv[1], "%d", &end);
+        sscanf(argv[1], "%d", &end);
     }
     else {
-		sscanf(argv[1], "%d", &start);
-		sscanf(argv[2], "%d", &end);
-     	if (argc == 4) {
-     		sscanf(argv[3], "%d", &step);
-     	}
-     	if (!step || start < end && step < 0)
-     		return 0;
+        sscanf(argv[1], "%d", &start);
+        sscanf(argv[2], "%d", &end);
+        if (argc == 4) {
+            sscanf(argv[3], "%d", &step);
+        }
+        if (!step || start < end && step < 0)
+            return 0;
     }
     for (int i = start; i < end; i += step)
-    	printf("%d\n", i);
+        printf("%d\n", i);
 }
